88f805b155fa18161dda9290bc53829781c17083
AleksandarBoev <aleksandarboev95@gmail.com>
2024-12-17 11:19:40 +0200

    Initial Commit - Spring Boot Setup

---
+++ src/main/java/dev/aleksandarboev/rollplangamebe/RollPlanGameBeApplication.java
@@ -0,0 +1,13 @@
+package dev.aleksandarboev.rollplangamebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollPlanGameBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RollPlanGameBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aleksandarboev/rollplangamebe/RollPlanGameBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aleksandarboev.rollplangamebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollPlanGameBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
